0c5450bff796f6ac9e47fe83d2c8c286c15712e7
HiimDManh <vdmanh1302@gmail.com>
2024-11-16 11:08:32 +0700

    Update layout

---
+++ BooksWebsite/Controllers/AIChattingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BooksWebsite.Controllers
+{
+    public class AIChattingController : Controller
+    {
+        // GET: AIChatting
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- BooksWebsite/assets/js/Before_Start/before_start.js
+++ BooksWebsite/assets/js/Before_Start/before_start.js
@@ -2,30 +2,66 @@
 
 var page = 0;
 
+function next_page() {
+    page++;
+    $('.page-question').css('transform', `translateX(-${100 * page}vw)`);
+    if (page > 0) {
+        $('.previous-btn').attr("hidden", false);
+    }
+    if (page == 4) {
+        $(".submit-btn").attr("hidden", false);
+        $(".next-btn").attr("hidden", true);
+    }
+}
+
+function previous_page() {
+    page--;
+    if (page < 1) {
+        $('.previous-btn').attr("hidden", true);
+    }
+    $('.page-question').css('transform', `translateX(-${100 * page}vw)`);
+    if (page == 3) {
+        $(".submit-btn").attr("hidden", true);
+        $(".next-btn").attr("hidden", false);
+    }
+    if (page > 0) {
+        $('.previous-btn').attr("hidden", false);
+    }
+}
+
 $(document).ready(() => {
     $("#nextBtn").on("click", () => {        
-        page++;
-        $('.page-question').css('transform', `translateX(-${100 * page}vw)`);
-        if (page > 0) {
-            $('.previous-btn').attr("hidden", false);
-        } 
-        if (page == 4) {
-            $(".submit-btn").attr("hidden", false);
-            $(".next-btn").attr("hidden", true);
-        }
-    });    
+        next_page();
+    });   
+    
     $("#previousBtn").on("click", () => {
-        page--;
-        if (page < 1) {
-            $('.previous-btn').attr("hidden", true);
-        }
-        $('.page-question').css('transform', `translateX(-${100 * page}vw)`);
-        if (page == 3) {
-            $(".submit-btn").attr("hidden", true);
-            $(".next-btn").attr("hidden", false);
-        }
-        if (page > 0) {
-            $('.previous-btn').attr("hidden", false);
-        }
+        previous_page();
     });
+
+    $(".answer-container").on('click', () => {
+        if (page < 4)
+            next_page();
+        else {
+            Swal.fire({
+                allowOutsideClick: false,
+                text: "Xác nhận hoàn thành?",
+                icon: "question",
+                buttonsStyling: false,
+                showCancelButton: true,
+                confirmButtonText: "Xác nhận",
+                cancelButtonText: "Huỷ",
+                customClass: {
+                    confirmButton: "btn btn-primary",
+                    cancelButton: "btn btn-danger"
+                }
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    window.location.href = "/";
+                }
+                else {
+                    Swal.close();
+                }
+            });
+        }
+    })
 });
